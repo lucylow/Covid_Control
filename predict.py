ee9edfeeb36a4394c522b82b0d39c5c55b14f375
づ｡◕‿◕｡)づ 盧露 <low.lucyy@gmail.com>
2020-12-22 22:34:43 -0500

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,51 @@
+# Copyright 2020 (c) Cognizant Digital Business, Evolutionary AI. All rights reserved. Issued under the Apache 2.0 License.
+
+import argparse
+
+
+def predict(start_date: str,
+            end_date: str,
+            path_to_ips_file: str,
+            output_file_path) -> None:
+    """
+    Generates and saves a file with daily new cases predictions for the given countries, regions and intervention
+    plans, between start_date and end_date, included.
+    :param start_date: day from which to start making predictions, as a string, format YYYY-MM-DDD
+    :param end_date: day on which to stop making predictions, as a string, format YYYY-MM-DDD
+    :param path_to_ips_file: path to a csv file containing the intervention plans between inception date (Jan 1 2020)
+     and end_date, for the countries and regions for which a prediction is needed
+    :param output_file_path: path to file to save the predictions to
+    :return: Nothing. Saves the generated predictions to an output_file_path CSV file
+    with columns "CountryName,RegionName,Date,PredictedDailyNewCases"
+    """
+    # !!! YOUR CODE HERE !!!
+    raise NotImplementedError
+
+
+# !!! PLEASE DO NOT EDIT. THIS IS THE OFFICIAL COMPETITION API !!!
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--start_date",
+                        dest="start_date",
+                        type=str,
+                        required=True,
+                        help="Start date from which to predict, included, as YYYY-MM-DD. For example 2020-08-01")
+    parser.add_argument("-e", "--end_date",
+                        dest="end_date",
+                        type=str,
+                        required=True,
+                        help="End date for the last prediction, included, as YYYY-MM-DD. For example 2020-08-31")
+    parser.add_argument("-ip", "--interventions_plan",
+                        dest="ip_file",
+                        type=str,
+                        required=True,
+                        help="The path to an intervention plan .csv file")
+    parser.add_argument("-o", "--output_file",
+                        dest="output_file",
+                        type=str,
+                        required=True,
+                        help="The path to the CSV file where predictions should be written")
+    args = parser.parse_args()
+    print(f"Generating predictions from {args.start_date} to {args.end_date}...")
+    predict(args.start_date, args.end_date, args.ip_file, args.output_file)
+    print("Done!")
